9da3be69496e047ddd7ec568811429f19617013c
ll <1243500742@qq.com>
2024-11-26 11:12:21 +0800

    怎么到撤回某个版本批处理

---
+++ 撤回到对应版本.cmd
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\git.exe" reset --hard 58fa47e9
+pause
