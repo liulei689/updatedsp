912376dcb5ad13673533ea538880521d62342061
ll <1243500742@qq.com>
2025-02-11 10:39:03 +0800

    11

--- AFWDPPS.3D/AFWDPPS.3D/View/Dome1.xaml.cs
+++ AFWDPPS.3D/AFWDPPS.3D/View/Dome1.xaml.cs
@@ -29,16 +29,15 @@ namespace WpfApp3D
             InitializeComponent();
 
             // 动态添加 BoxVisual3D
-            boxModel = new BoxVisual3D
+            boxModel1 = new BoxVisual3D
             {
                 Center = new Point3D(0, 0, 0),
                 Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#f9e2b8")), // 使用自定义颜色
-                Length = 50, // 调整尺寸
-                Width = 50,  // 调整尺寸
+                Length = 20, // 调整尺寸
+                Width = 15,  // 调整尺寸
                 Height = 1.8 // 调整尺寸
             };
-            viewport.Items.Add(boxModel); // 添加到 HelixViewport3D
-            boxModel1 = new BoxVisual3D
+            boxModel = new BoxVisual3D
             {
                 Center = new Point3D(0, 0, 20),
                 Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#f9e2b8")), // 使用自定义颜色
@@ -46,18 +45,19 @@ namespace WpfApp3D
                 Width = 30,  // 调整尺寸
                 Height = 1.8 // 调整尺寸
             };
-
             viewport.Items.Add(boxModel1); // 添加到 HelixViewport3D
-                                           // 创建圆柱的顶点和三角形索引
-                                           // 定义弹簧的参数
-                                           // 定义弹簧的参数
+
+            viewport.Items.Add(boxModel); // 添加到 HelixViewport3D
+                                          // 创建圆柱的顶点和三角形索引
+                                          // 定义弹簧的参数
+                                          // 定义弹簧的参数
             Point3D startPoint = boxModel.Center; // 起点
             Point3D endPoint = boxModel1.Center; // 终点
             double radius = 0.5; // 弹簧的半径
             int turns = 20; // 弹簧的圈数
 
             // 创建弹簧几何形状
-            var springMesh = CreateSpringGeometry(startPoint, endPoint, radius, turns);
+            var springMesh = CreateSpringGeometry(endPoint, startPoint, radius, turns);
 
             // 创建几何模型
             springModel = new GeometryModel3D
@@ -93,6 +93,8 @@ namespace WpfApp3D
             timer11.Interval = TimeSpan.FromMilliseconds(1000); // 每500毫秒更新一次
             timer11.Tick += Timer_Tick11;
             timer11.Stop();
+            data();
+            ToggleTimer_Click(null, null);
         }
         private MeshGeometry3D CreateSpringGeometry(Point3D startPoint, Point3D endPoint, double radius, int turns)
         {
@@ -232,11 +234,12 @@ namespace WpfApp3D
                 timer11.Stop();
                 // 启动定时器
                 timer = new DispatcherTimer();
-                timer.Interval = TimeSpan.FromMilliseconds(100); // 每500毫秒更新一次
+                timer.Interval = TimeSpan.FromMilliseconds(10); // 每500毫秒更新一次
                 timer.Tick += Timer_Tick;
                 timer.Start();
                 isTimerRunning = true;
-                ((Button)sender).Content = "停止定时器";
+                if (sender != null)
+                    ((Button)sender).Content = "停止定时器";
             }
             else
             {
@@ -248,20 +251,133 @@ namespace WpfApp3D
         }
         private int currentIndex = 0; // 当前索引
         private bool issuiji = false;
+        List<(double x, double y)> dataList;
+        private void data()
+        {
+            // 创建一个List来存储数据
+            dataList = new List<(double x, double y)>
+        {
+           (0,0),
+(0.0471,0.4707),
+(0.094,0.9395),
+(0.1405,1.4047),
+(0.1864,1.8642),
+(0.2316,2.3165),
+(0.276,2.7596),
+(0.3192,3.1918),
+(0.3611,3.6115),
+(0.4017,4.0169),
+(0.4406,4.4065),
+(0.4779,4.7787),
+(0.5132,5.132),
+(0.5465,5.4651),
+(0.5777,5.7767),
+(0.6066,6.0655),
+(0.633,6.3304),
+(0.657,6.5703),
+(0.6784,6.7844),
+(0.6972,6.9717),
+(0.7131,7.1314),
+(0.7263,7.2631),
+(0.7366,7.3662),
+(0.744,7.4402),
+(0.7485,7.4848),
+(0.75,7.5),
+(0.7486,7.4856),
+(0.7442,7.4417),
+(0.7368,7.3684),
+(0.7266,7.2661),
+(0.7135,7.1351),
+(0.6976,6.976),
+(0.6789,6.7895),
+(0.6576,6.5761),
+(0.6337,6.3368),
+(0.6073,6.0725),
+(0.5784,5.7843),
+(0.5473,5.4733),
+(0.5141,5.1407),
+(0.4788,4.7879),
+(0.4416,4.4161),
+(0.4027,4.027),
+(0.3622,3.6219),
+(0.3203,3.2026),
+(0.2771,2.7707),
+(0.2328,2.3278),
+(0.1876,1.8758),
+(0.1416,1.4164),
+(0.0951,0.9514),
+(0.0483,0.4826),
+(0.0012,0.0119),
+(-0.0459,-0.4588),
+(-0.0928,-0.9277),
+(-0.1393,-1.3929),
+(-0.1853,-1.8527),
+(-0.2305,-2.3051),
+(-0.2748,-2.7485),
+(-0.3181,-3.181),
+(-0.3601,-3.601),
+(-0.4007,-4.0068),
+(-0.4397,-4.3968),
+(-0.4769,-4.7694),
+(-0.5123,-5.1233),
+(-0.5457,-5.457),
+(-0.5769,-5.7691),
+(-0.6058,-6.0585),
+(-0.6324,-6.324),
+(-0.6565,-6.5646),
+(-0.6779,-6.7793),
+(-0.6967,-6.9672),
+(-0.7128,-7.1277),
+(-0.726,-7.2601),
+(-0.7364,-7.3639),
+(-0.7439,-7.4387),
+(-0.7484,-7.4841),
+(-0.75,-7.5),
+(-0.7486,-7.4863),
+(-0.7443,-7.4431),
+(-0.7371,-7.3706),
+(-0.7269,-7.269),
+(-0.7139,-7.1388),
+(-0.698,-6.9804),
+(-0.6795,-6.7945),
+(-0.6582,-6.5818),
+(-0.6343,-6.3432),
+(-0.608,-6.0795),
+(-0.5792,-5.7919),
+(-0.5481,-5.4815),
+(-0.5149,-5.1494),
+(-0.4797,-4.797),
+(-0.4426,-4.4258),
+(-0.4037,-4.037),
+(-0.3632,-3.6324),
+(-0.3213,-3.2134),
+(-0.2782,-2.7818),
+(-0.2339,-2.3392),
+(-0.1887,-1.8874),
+(-0.1428,-1.4281),
+(-0.0963,-0.9632),
+(-0.0495,-0.4945),
+(-0.0024,-0.0239)
+        };
+        }
+
         // 定时器事件
         private void Timer_Tick(object sender, EventArgs e)
         {
-            Random random = new Random();
-            yaw += random.Next(-15, 15); // 随机生成 -5 到 5 的角度变化量
-            pitch += random.Next(-15, 15); // 随机生成 -5 到 5 的角度变化量
+            //Random random = new Random();
+            //yaw += random.Next(-15, 15); // 随机生成 -5 到 5 的角度变化量
+            //pitch += random.Next(-15, 15); // 随机生成 -5 到 5 的角度变化量
 
-            // 限制角度范围
-            yaw = Clamp(yaw, -15, 15);
-            pitch = Clamp(pitch, -15, 15);
-
-            yawTextBox.Text = yaw.ToString("F2");
-            pitchTextBox.Text = pitch.ToString("F2");
+            //// 限制角度范围
+            //yaw = Clamp(yaw, -15, 15);
+            //pitch = Clamp(pitch, -15, 15);
+            currentIndex = (currentIndex + 1) % dataList.Count;
+            yawTextBox.Text = dataList[currentIndex].x.ToString("F2");
+            pitchTextBox.Text = dataList[currentIndex].y.ToString("F2");
+            yaw = dataList[currentIndex].x;
+            pitch = dataList[currentIndex].y;
             UpdateTransform();
+
 
         }
 
@@ -280,8 +396,9 @@ namespace WpfApp3D
         }
         private void UpdateSpringGeometry(BoxVisual3D box1, BoxVisual3D box2, GeometryModel3D springModel)
         {
-            // 获取新的起点和终点
+            // 弹簧新的起点和终点在这里动态调整
             Point3D startPoint = box1.Center;
+            startPoint.Offset(yaw, yaw, 0);
             Point3D endPoint = box2.Center;
 
             // 计算弹簧的方向和长度
@@ -290,7 +407,7 @@ namespace WpfApp3D
 
             // 生成一个小的随机偏移量
             Random rand = new Random();
-            double randomOffset = (rand.NextDouble() - 0.5) * 0.8; // 随机偏移量，范围在 -0.05 到 0.05 之间
+            double randomOffset = (rand.NextDouble() - 0.5) * 0.2; // 随机偏移量，范围在 -0.05 到 0.05 之间
 
             // 创建一个平移变换，让弹簧稍微动一下
             Vector3D moveVector = direction * randomOffset;
@@ -311,6 +428,8 @@ namespace WpfApp3D
             var pitchRotation1 = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), pitch * 0.1));
             boxModel1.Transform = new Transform3DGroup { Children = { yawRotation1, pitchRotation1 } };
             UpdateSpringGeometry(boxModel, boxModel1, springModel);
+            x1.Text = (yaw * 0.1).ToString();
+            y1.Text = (pitch * 0.1).ToString();
         }
 
         private void openclosecom_Click(object sender, RoutedEventArgs e)
