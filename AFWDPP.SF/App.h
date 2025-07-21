e6ec4933a77583cc84018c8abbcb559191def9d6
ll <1243500742@qq.com>
2025-07-21 09:48:19 +0800

    1

--- AFWDPP.SF/App.h
+++ AFWDPP.SF/App.h
@@ -10,4 +10,4 @@ typedef unsigned long long Uint64;
 typedef float              float32;
 typedef long double        float64;
 #define Dll_API __declspec(dllexport)
-extern "C" Dll_API Uint8 add(Uint8 a, Uint8 b);
+extern "C" Dll_API Uint8 add(Uint8 a, Uint8 b); //不加 extern "C"	C++ 会改编函数名，C# 找不到（除非你知道改编后的名字，用 EntryPoint = "?add@@YAEEE@Z"，不现实）。
